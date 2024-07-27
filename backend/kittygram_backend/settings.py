56c83422163a0420f6e0195e1975bd39dfa86085
drongozelenograd <drongozel@yandex.ru>
2024-07-27 08:19:47 +0000

    Final commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,11 +3,14 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+import environ
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env('SECRET_KEY', default='default-secret-key')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.92.121', '127.0.0.1', 'localhost', 'kittygram-dz.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +93,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
